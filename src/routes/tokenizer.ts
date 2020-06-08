b2597269f2dbebf64931027a1696bdf3e6116d8f
Edward Pasenidis <me@edpasenidis.tech>
2020-06-08 15:40:33 +0300

    refractor: remove useless import

--- src/routes/tokenizer.ts
+++ src/routes/tokenizer.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
-import auth from '../middleware/authToken';
 import generateAccessToken from '../utils/generateToken';
 
 const router = express.Router();
 const parser = bodyParser.json();
 
 router.get('/', parser, async (req: Request, res: Response) => {
-    if (!req.body.username || !req.body.password) return res.json({ message: "Missing 'user' query" });
+    if (!req.body.username || !req.body.password) return res.json({ message: "Missing 'username' query" });
     const token = generateAccessToken(req.body.username);
     res.json(token);
 });
